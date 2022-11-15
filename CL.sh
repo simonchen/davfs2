9ea4a6e9874f80b70b709575754d9220dd4c9cc1
xychenxy <chenxinyi1978@hotmail.com>
2022-11-15 19:30:41 +0800

    Update CL.sh

--- CL.sh
+++ CL.sh
@@ -3,7 +3,7 @@ tar -xzvf ./musl-1.2.3.tar.gz -C . &&
 tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
 tar -xzvf ./expat-2.5.0.tar.gz -C . &&
 tar -xzvf ./neon-0.31.0.tar.gz -C . &&
-./musl-1.2.3/configure && make install && 
+./musl-1.2.3/configure --host=$HOST && make install && 
 ./zilib-1.2.11/configure --includedir="/usr/local/musl/include" --libdir="/usr/local/musl/lib" --static --sharedlibdir=. && make install &&
 export LDFLAGS=-Wl,-L/usr/local/musl/lib,-lexpat &&
 export CPPFLAGS=-I/usr/local/musl/include &&
