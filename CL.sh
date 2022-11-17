d1979f85381bee8ab60d165a1e4b3602d33cd8fe
xychenxy <chenxinyi1978@hotmail.com>
2022-11-17 10:45:49 +0800

    Update CL.sh

--- CL.sh
+++ CL.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
-
-#tar -xzvf ./musl-1.2.3.tar.gz -C . &&
-#tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
-tar -xzvf ./expat-2.5.0.tar.gz -C . &&
-#tar -xzvf ./neon-0.31.0.tar.gz -C . &&
-
-#./musl-1.2.3/configure --host=$HOST && make install &&
-#./zlib-1.2.11/configure --includedir="/usr/local/musl/include" --libdir="/usr/local/musl/lib" --static --sharedlibdir=. && make install &&
-echo "\n================ EXPAT COMPLATION =================\n"
+echo "\n================ ENVIROMENT ========================\n"
 env
-cd ./expat-2.5.0 && ./configure --enable-static --host=$ENV_HOST && make install && cd .. 
-#export LDFLAGS=-Wl,-L/usr/local/musl/lib,-lexpat &&
-#export CPPFLAGS=-I/usr/local/musl/include &&
-#./configure --prefix=/usr/local/musl --with-expat --enable-static --host=$HOST && make install &&
-#ln -s /usr/local/musl/bin/neon-config /usr/local/bin/neon-config &&
-#./configure CC="/usr/local/musl/bin/musl-gcc" CFLAGS="-I/user/local/musl/include -I/usr/local/musl/include/neon" CPPFLAGS="-I/usr/local/musl/include -I/usr/local/musl/include/neon" LDFLAGS="-Wl,-Bstatic -L/usr/local/musl/lib" --prefix=/usr/local/musl --host=$HOST && makeinstall
+
+echo "\n================ Extracting dependent packages =====\n"
+tar -xzvf ./musl-1.2.3.tar.gz -C . &&
+tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
+tar -xzvf ./expat-2.5.0.tar.gz -C . &&
+tar -xzvf ./neon-0.31.0.tar.gz -C . &&
+
+echo "\n================ MUSL COMPLATION =================\n"
+./musl-1.2.3/configure --host=$HOST && make install &&
+
+echo "\n================ ZLIB COMPLATION =================\n"
+./zlib-1.2.11/configure --includedir="/usr/local/musl/include" --libdir="/usr/local/musl/lib" --static --sharedlibdir=. && make install &&
+
+echo "\n================ EXPAT COMPLATION =================\n"
+./expat-2.5.0/configure --enable-static --host=$ENV_HOST && make install && cd .. 
+
+echo "\n================ neon COMPLATION =================\n"
+export LDFLAGS=-Wl,-L/usr/local/musl/lib,-lexpat &&
+export CPPFLAGS=-I/usr/local/musl/include &&
+./neon-0.31.0/configure --prefix=/usr/local/musl --with-expat --enable-static --host=$HOST && make install &&
+ln -s /usr/local/musl/bin/neon-config /usr/local/bin/neon-config &&
+
+echo "\n================ DAVFS2 COMPLATION =================\n"
+chmod +x ./configure
+./configure CC="/usr/local/musl/bin/musl-gcc" CFLAGS="-I/user/local/musl/include -I/usr/local/musl/include/neon" CPPFLAGS="-I/usr/local/musl/include -I/usr/local/musl/include/neon" LDFLAGS="-Wl,-Bstatic -L/usr/local/musl/lib" --prefix=/usr/local/musl --host=$HOST && makeinstall
+
 echo "\n================ DONE =============================\n"
