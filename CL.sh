9664994446597b548fa3d677b8fbe40771b8766b
xychenxy <chenxinyi1978@hotmail.com>
2022-11-17 10:28:31 +0800

    Update CL.sh

--- CL.sh
+++ CL.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-HOST=$1
 
 #tar -xzvf ./musl-1.2.3.tar.gz -C . &&
 #tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
 tar -xzvf ./expat-2.5.0.tar.gz -C . &&
 #tar -xzvf ./neon-0.31.0.tar.gz -C . &&
-echo "$HOST"
+
 #./musl-1.2.3/configure --host=$HOST && make install &&
 #./zlib-1.2.11/configure --includedir="/usr/local/musl/include" --libdir="/usr/local/musl/lib" --static --sharedlibdir=. && make install &&
 echo "\n================ EXPAT COMPLATION =================\n"
-cd ./expat-2.5.0 && ./configure --host=mipsel-unknown-linux-gnu && make install && cd .. 
+env
+cd ./expat-2.5.0 && ./configure --enable-static --host=$HOST && make install && cd .. 
 #export LDFLAGS=-Wl,-L/usr/local/musl/lib,-lexpat &&
 #export CPPFLAGS=-I/usr/local/musl/include &&
 #./configure --prefix=/usr/local/musl --with-expat --enable-static --host=$HOST && make install &&
