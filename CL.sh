db54c8559778742e34eeb689426fdb19ba0e0bb4
xychenxy <chenxinyi1978@hotmail.com>
2022-11-17 09:49:17 +0800

    Update CL.sh

--- CL.sh
+++ CL.sh
@@ -2,18 +2,18 @@
 HOST=$1
 echo "$HOST"
 
-tar -xzvf ./musl-1.2.3.tar.gz -C . &&
-tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
+#tar -xzvf ./musl-1.2.3.tar.gz -C . &&
+#tar -xzvf ./zlib-1.2.11.tar.gz -C . &&
 tar -xzvf ./expat-2.5.0.tar.gz -C . &&
-tar -xzvf ./neon-0.31.0.tar.gz -C . &&
+#tar -xzvf ./neon-0.31.0.tar.gz -C . &&
 
 #./musl-1.2.3/configure --host=$HOST && make install &&
 #./zlib-1.2.11/configure --includedir="/usr/local/musl/include" --libdir="/usr/local/musl/lib" --static --sharedlibdir=. && make install &&
-echo "================ EXPAT COMPLATION ================="
+echo "\n================ EXPAT COMPLATION =================\n"
 cd ./expat-2.5.0 && ./configure --host=$HOST && make install && cd .. 
 #export LDFLAGS=-Wl,-L/usr/local/musl/lib,-lexpat &&
 #export CPPFLAGS=-I/usr/local/musl/include &&
 #./configure --prefix=/usr/local/musl --with-expat --enable-static --host=$HOST && make install &&
 #ln -s /usr/local/musl/bin/neon-config /usr/local/bin/neon-config &&
 #./configure CC="/usr/local/musl/bin/musl-gcc" CFLAGS="-I/user/local/musl/include -I/usr/local/musl/include/neon" CPPFLAGS="-I/usr/local/musl/include -I/usr/local/musl/include/neon" LDFLAGS="-Wl,-Bstatic -L/usr/local/musl/lib" --prefix=/usr/local/musl --host=$HOST && makeinstall
-echo "================ DONE ============================="
+echo "\n================ DONE =============================\n"
